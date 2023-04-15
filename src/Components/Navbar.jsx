69b1355cfc9574e4ef0a560549664ee33bc484ac
vivekkhade23 <khadev881@gmail.com>
2023-04-16 02:15:51 +0530

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { HStack } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <HStack>
+        <Link to="/">
+            Home
+        </Link>
+        <Link to="/leaderboard">
+            LeaderBoard
+        </Link>
+    </HStack>
   )
 }
 
