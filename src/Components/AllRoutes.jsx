009003d1bf1bda35d3a9bf610bd03f9361a8726b
vivekkhade23 <khadev881@gmail.com>
2023-04-16 02:00:27 +0530

    Routes

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Page/Home'
+import Play from '../Page/Play'
+import Leaderboard from '../Page/Leaderboard'
 
 const AllRoutes = () => {
   return (
 <Routes>
 <Route path='/' element={<Home/>}/>
-</Routes>  )
+<Route path='/play' element={<Play/>}/>
+<Route path='/leaderboard' element={<Leaderboard/>}/>
+
+</Routes> 
+ )
 }
 
 export default AllRoutes
